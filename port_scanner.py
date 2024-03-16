436a69acf7e6c06fe255043f11ce1b0e3221463b
Hackhoven <142750639+Hackhoven@users.noreply.github.com>
2024-03-17 01:37:45 +0400

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -2,14 +2,11 @@
 
 import socket
 import re
-import threading
 from concurrent.futures import ThreadPoolExecutor
 
 #COMMON_PORTS = [21, 22, 23, 25, 53, 80, 443, 3306, 5432, 8080]
 
-
 def validate_host(target_host):
-
     try:
         socket.inet_aton(target_host)   #validate IP
         return True
@@ -18,37 +15,29 @@ def validate_host(target_host):
 
     if re.match(r'^[a-zA-Z0-9.-]+$', target_host):
         return True
-    
     return False
 
-
-def scan_port(target_host, port):
-
+def scan_port(target_host, port, timeout=1):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.settimeout(1)
+            s.settimeout(timeout)
             result = s.connect_ex((target_host, port))
-            
             if result == 0:
-                print(f"Port {port} is open")
-    
+                service_name = socket.getservbyport(port)
+                print(f"Port {port} ({service_name}) is open")
     except Exception as e:
         print(f"Error scanning port {port}: {e}")
 
-
-def scan_ports(target_host, max_threads=20):
-
+def scan_ports(target_host, max_threads=20, timeout=1):
     if not validate_host(target_host):
         print("Invalid target host. Please enter a valid IP address or hostname.")
         return
-    
-    print(f"Scanning ports for {target_host}...")
 
+    print(f"Scanning ports for {target_host}...")
     with ThreadPoolExecutor(max_workers=max_threads) as executor:
         #for port in COMMON_PORTS:
-        for port in range(1, 10000):
-            executor.submit(scan_port, target_host, port)
-
+        for port in range(1, 1000):
+            executor.submit(scan_port, target_host, port, timeout)
 
 if __name__ == "__main__":
     target_host = input("Enter the target host: ")
