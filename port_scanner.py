2c61d476e862e26fc61f04b811d839d3a6315f7c
Hackhoven <142750639+Hackhoven@users.noreply.github.com>
2024-03-17 01:17:16 +0400

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -1,18 +1,20 @@
-#Hackhoven's port scanner
+# Hackhoven's port scanner
 
 import socket
 import re
 import threading
+from concurrent.futures import ThreadPoolExecutor
+
+#COMMON_PORTS = [21, 22, 23, 25, 53, 80, 443, 3306, 5432, 8080]
 
 
 def validate_host(target_host):
+
     try:
         socket.inet_aton(target_host)   #validate IP
         return True
-    
     except socket.error:
         pass
-
 
     if re.match(r'^[a-zA-Z0-9.-]+$', target_host):
         return True
@@ -21,42 +23,31 @@ def validate_host(target_host):
 
 
 def scan_port(target_host, port):
+
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(1)
-
-        result = s.connect_ex((target_host, port))
-
-        if result == 0:
-            print(f"Port {port} is open")
-
-        s.close()
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.settimeout(1)
+            result = s.connect_ex((target_host, port))
+            
+            if result == 0:
+                print(f"Port {port} is open")
     
-    except socket.error as e:
-        print(f"Socket error: {e}")
-
-    except socket.gaierror as ge:
-        print(f"Hostname resolution error: {ge}")
-
-    except Exception as err:
-        print(f"Error occurred: {err}")
+    except Exception as e:
+        print(f"Error scanning port {port}: {e}")
 
 
-def scan_ports(target_host):
-    
+def scan_ports(target_host, max_threads=20):
+
     if not validate_host(target_host):
         print("Invalid target host. Please enter a valid IP address or hostname.")
         return
+    
+    print(f"Scanning ports for {target_host}...")
 
-    threads = []
-    for port in range(1, 65536):
-        thread = threading.Thread(target=scan_port, args=(target_host, port))
-        thread.start()
-        threads.append(thread)
-
-    for thread in threads:
-        thread.join()
-
+    with ThreadPoolExecutor(max_workers=max_threads) as executor:
+        #for port in COMMON_PORTS:
+        for port in range(1, 10000):
+            executor.submit(scan_port, target_host, port)
 
 
 if __name__ == "__main__":
